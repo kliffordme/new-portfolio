8fd87c2e42525781aeb7aa3531ee73464cb3c173
klifford-singlifeph <klifford@singlife.com>
2025-02-19 19:22:04 +0800

    Fix the structure so the images would show

--- src/main.js
+++ src/main.js
@@ -60,12 +60,12 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('./space.jpg');
 scene.background = spaceTexture;
 
 // Avatar
 
-const fordTexture = new THREE.TextureLoader().load('ford.jpg');
+const fordTexture = new THREE.TextureLoader().load('/ford.jpg');
 
 const ford = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: fordTexture }));
 
@@ -73,8 +73,8 @@ scene.add(ford);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('/normal.jpg');
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
